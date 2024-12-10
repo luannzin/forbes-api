59e8dfe8894bb69246f34250d5d9681c870c285a
luannzin <luandaniel966@gmail.com>
2024-12-10 11:41:08 -0300

    feat: add new bun-vercel package to run this api on vercel

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,19 @@ import { json } from "@stricjs/app/send";
 import { Router } from "@stricjs/router";
 import { extractForbesData } from "./utils/extract";
 
-export default new Router().get("/:profile", async (ctx) => {
-  const data = await extractForbesData(ctx.params.profile);
+export default new Router()
+  .get("/", async () => {
+    return json({
+      made: "by luannzin",
+      guide: {
+        "/:profile": "Get the profile data from Forbes",
+      },
+    });
+  })
+  .get("/:profile", async (ctx) => {
+    const data = await extractForbesData(ctx.params.profile);
 
-  return json({
-    ...data,
+    return json({
+      ...data,
+    });
   });
-});
