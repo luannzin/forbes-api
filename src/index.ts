199b6c47277977c322844c32b55a8d95e138ca20
luannzin <luandaniel966@gmail.com>
2024-12-10 12:02:18 -0300

    feat: add new endpoint for the 10 richest people

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { json } from "@stricjs/app/send";
 import { Router } from "@stricjs/router";
 import { extractForbesData } from "./utils/extract";
+import axios from "axios";
 
 export default new Router()
   .get("/", async () => {
@@ -8,6 +9,7 @@ export default new Router()
       made: "by luannzin",
       guide: {
         "/:profile": "Get the profile data from Forbes",
+        "/list": "Get the top 10 richest people",
       },
     });
   })
@@ -17,4 +19,18 @@ export default new Router()
     return json({
       ...data,
     });
+  })
+  .get("/list", async () => {
+    try {
+      const { data } = await axios.get(
+        "https://www.forbes.com/forbesapi/person/rtb/0/-estWorthPrev/true.json?fields=rank,uri,personName,lastName,gender,source,industries,countryOfCitizenship,birthDate,finalWorth,estWorthPrev,imageExists,squareImage,listUri&limit=10"
+      );
+
+      return json([...data.personList.personsLists]);
+    } catch (e) {
+      const err = e as Error;
+      return json({
+        error: err.message,
+      });
+    }
   });
